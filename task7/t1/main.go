ae8d4ed4a241f1a2794e88b9817e675c365d9d93
gitea <d.bulavin81@yandex.ru>
2021-08-11 15:47:50 +0300

    task7

--- task7/t1/main.go
+++ task7/t1/main.go
@@ -3,29 +3,39 @@ package main
 import (
 	"fmt"
 	"reflect"
+	"strings"
 )
 
-func t1(sql string, v ...interface{}) {
-	fmt.Println(v)
-	var vv []interface{}
-	for i := 0; i < len(v); i++ {
-		rv := reflect.TypeOf(v[i]).Kind()
-		if rv == reflect.Bool {
-			vv = append(vv, v[i])
-		}
-		if rv == reflect.Int {
-			vv = append(vv, v[i])
-		}
-		if rv == reflect.Slice {
+func convValuesToSlice(s string, v ...interface{}) (string, []interface{}) {
 
+	var result []interface{}
+	f := strings.Split(s, "?")
+
+	for i := 0; i < len(v); i++ {
+		switch reflect.TypeOf(v[i]).Kind() {
+		case reflect.Slice, reflect.Array:
+			sv := reflect.ValueOf(v[i])
+			for z := 0; z < sv.Len(); z++ {
+				result = append(result, sv.Index(z).Interface())
+				if z == sv.Len()-1 {
+					f[i] = f[i] + "?"
+				} else {
+					f[i] = f[i] + "?,"
+				}
+			}
+		default:
+			f[i] = f[i] + "?"
+			result = append(result, v[i])
 		}
 	}
-	fmt.Println(vv)
+	res := strings.Join([]string{f[0], f[1], f[2]}, "")
+
+	return res, result
 
 }
 
 func main() {
-
-	t1("SELECT * FROM table WHERE deleted = ? AND id IN(?) AND count < ?", false, []int{1, 6, 234}, 555)
-
+	//"SELECT * FROM table WHERE deleted = ? AND id IN(?) AND count < ?",
+	str, value := convValuesToSlice("SELECT * FROM table WHERE deleted = ? AND id IN(?) AND count < ?", false, []int{1, 6, 234}, 555)
+	fmt.Println(str, value)
 }
---
+++ task7/t2/main.go
@@ -0,0 +1,14 @@
+package main
+
+type User struct {
+	ID        int64
+	FirstName string
+	LastName  string
+	Email     string
+	Age       int
+	Admin     bool
+}
+
+func main() {
+
+}
